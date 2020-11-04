ef3cc787a52a796b8bd6593431d43fade2701239
Charles M Hollins <68667434+Sp33dRunn3r@users.noreply.github.com>
2020-11-04 15:23:26 -0500

    Create Emt_Main_Function.py

---
+++ Emt_Main_Function.py
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------------------------------------------->
+def Initiator(): #(Program initiator)
+    Survey_taker_num = input('''
+    -----> If you would like to exit or close the program, press 'Q'
+
+    -----> How many people would you like to survey today? ''') #(Question for survey amount)
+    while Survey_taker_num == "" or Survey_taker_num.isalpha():
+        Survey_taker_num = input('Please enter a valid response... ')
+
+    for x in range(int(Survey_taker_num)): #(Statistics printed and number of surveys updated)
+        feel_question()
+        dict['People_Surveyed'] = x + 1
+        print(f'''
+
+    -----> Name: {dict['Name']}
+    -----> Emotional value for today: {dict['Emotion']}
+    -----> Number of people surveyed: {dict['People_Surveyed']}
+
+        ''')
