82323624017b43c73026220c534f0cb56397e140
Charles M Hollins <68667434+Sp33dRunn3r@users.noreply.github.com>
2020-11-04 15:16:30 -0500

    Create Values.py

---
+++ Values.py
@@ -0,0 +1,16 @@
+#(First set of numerical values coordinating with emotion for day)
+if int(qemtn) >= 8:
+                dict['key1'] = (qemtn)
+                dtrmn = print('So glad to hear your feeling well today!')
+            elif int(qemtn) <= 3:
+                dict['key1'] = (qemtn)
+                dtrmn = print('So sorry to hear your not feeling well today!')
+            elif int(qemtn) < 5:
+                dict['key1'] = (qemtn)
+                dtrmn = print('Sorry to hear your not feeling well.')
+            elif int(qemtn) == 5:
+                dict['key1'] = (qemtn)
+                dtrmn = print('Somewhere in the middle it seems... interesting.')
+            elif int(qemtn) > 5:
+                dict['key1'] = (qemtn)
+                dtrmn = print('Glad to hear your feeling well today.')
